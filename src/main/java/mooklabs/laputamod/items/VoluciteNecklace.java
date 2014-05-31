a289fc21c596b240ddcad137a28266a270909152
vicky <mookbot@gmail.com>
2014-05-31 21:17:50 +0200

    THE NECKLACE IS MINE, well it can change modes now! started robot(dont look at it yet)

--- src/main/java/mooklabs/laputamod/items/VoluciteNecklace.java
+++ src/main/java/mooklabs/laputamod/items/VoluciteNecklace.java
@@ -46,18 +46,13 @@ public class VoluciteNecklace extends Item {
 			String owner = itemStack.stackTagCompound.getString("owner");
 
 			if (itemStack.stackTagCompound.getBoolean("creativeSpawned")) list.add("Sole Owner: " + owner);
-
+			else list.add("Owner: " + owner);
 
 			if (owner.equals(player.getCommandSenderName())) {// if belongs to player
-				if (true) list.add(EnumChatFormatting.GREEN + "Grinder: " + "Researched");
-				else list.add(EnumChatFormatting.RED + "Grinder: " + "Not Researched");
+				list.add(EnumChatFormatting.GREEN + "Grinder: " + "Researched");
 
-				if (false) list.add(EnumChatFormatting.GREEN + "Furnace: " + "Researched");
-				else list.add(EnumChatFormatting.RED + "Furnace: " + "Not Researched");
-
-			} else {
-				list.add(EnumChatFormatting.RED + "This Necklace belongs to someone with power,"+
-								" return it to any admin since you cant use it");
+			} else {// if doesent belong to player
+				list.add(EnumChatFormatting.RED + "This Necklace belongs to someone with power," + " return it to any admin since you can't use it");
 			}
 		} else {// if has tags
 			list.add(EnumChatFormatting.BLUE + "NO INFO!");
@@ -78,20 +73,23 @@ public class VoluciteNecklace extends Item {
 		if (itemStack.stackTagCompound == null) {
 			itemStack.stackTagCompound = new NBTTagCompound();// create tag
 			itemStack.stackTagCompound.setBoolean("creativeSpawned", wasCrafted);
+			itemStack.stackTagCompound.setBoolean("launch", true);
+			itemStack.stackTagCompound.setBoolean("hover", true);
+
+			itemStack.stackTagCompound.setString("owner", player.getCommandSenderName());
 
 		} else {// has allready been registed
 
 			if (itemStack.stackTagCompound.getBoolean("creativeSpawned")) {
 				MLib.printToPlayer("Sorry this has restricted access by " + itemStack.stackTagCompound.getString("owner"));
-				return;//if it cannot be claimed by another player(ie for admins)
+				return;// if it cannot be claimed by another player(ie for admins)
 			} else {
-				//switch owner?
+				// switch owner?
 
 				itemStack.stackTagCompound.setString("owner", player.getCommandSenderName());// register to name
-				
-				
+
 			}
-			
+
 		}
 	}
 
@@ -101,20 +99,48 @@ public class VoluciteNecklace extends Item {
 	/**
 	 * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
 	 */
-	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
-		if (par3EntityPlayer.isSprinting()) {
-			Vec3 desiredDirection = par3EntityPlayer.getLookVec();
+	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+		if (itemStack.stackTagCompound == null) register(itemStack, player, false);
+		// NOT YET BECUAE ITS ITEM BASEDelse if (itemStack.stackTagCompound.getString("owner").equals(player.getCommandSenderName())) register(itemStack, player, false);
+		NBTTagCompound tagC = itemStack.stackTagCompound;
+		if (player.isSneaking()) {
+			String str = "";
+			switch (tagC.getString("mode")) {
+			case "launch":
+				str = "hover";
+				if (tagC.getBoolean("launch")) break;//bit iffy but will work later
+			case "hover":
+				str = "launch";
+				if (!tagC.getBoolean("hover")) break;
+			default:
+				str = "launch";
+			}
+			MLib.printToPlayer("Effect set to" + str);
 
-			par3EntityPlayer.motionX = 1.3 * desiredDirection.xCoord;
-			par3EntityPlayer.motionY = 1.3 * desiredDirection.yCoord;
-			par3EntityPlayer.motionZ = 1.3 * desiredDirection.zCoord;
-		} else if (par3EntityPlayer.isSneaking()) {
+			itemStack.stackTagCompound.setString("mode", str);
 
 		} else {
-			par3EntityPlayer.motionY = 0;
+			switch (tagC.getString("mode")) {
+			case "launch":
+				launchPlayer(player, 1.3);
+				break;
+			case "hover":
+				player.motionY = 0;
+				break;
+			default:
+				MLib.printToPlayer("what you do to the spellbook?");
+			}
 		}
 
-		return par1ItemStack;
+		return itemStack;
+	}
+
+	public void launchPlayer(EntityPlayer entityPlayer, double amt) {
+		Vec3 desiredDirection = entityPlayer.getLookVec();
+
+		entityPlayer.motionX = amt * desiredDirection.xCoord;
+		entityPlayer.motionY = amt * desiredDirection.yCoord;
+		entityPlayer.motionZ = amt * desiredDirection.zCoord;
 	}
 
 }
