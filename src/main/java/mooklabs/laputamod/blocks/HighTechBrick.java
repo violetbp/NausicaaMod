af0181f4fcc9e35b35af22d7a0884e189c831950
vicky <mookbot@gmail.com>
2014-05-31 15:13:03 +0200

    adding block

--- src/main/java/mooklabs/laputamod/blocks/HighTechBrick.java
+++ src/main/java/mooklabs/laputamod/blocks/HighTechBrick.java
@@ -13,18 +13,17 @@ import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 /**
- * dont mess with this please
- * 
+ * that suff that laputas made out of
  * @author vicky
  */
-public class VoluciteBlock extends Block {
+public class HighTechBrick extends Block {
 
-	public VoluciteBlock() {
+	public HighTechBrick() {
 		super(Material.iron);
 
-		setBlockName("voluciteBlock");
+		setBlockName("highTechBrick");
 		setStepSound(Block.soundTypeStone);
-		setBlockTextureName(LapMain.itemfold + ":voluciteOre");
+		setBlockTextureName(LapMain.itemfold + ":highTechBrick");
 		
 
 	}
@@ -38,28 +37,7 @@ public class VoluciteBlock extends Block {
 	 * Called upon block activation (right click on the block.)
 	 */
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
-		if (isConstructedCorrectly(world, x, y, z)) {
-			MLib.printToPlayer("Sucess!");
-			return true;
-		}
-		MLib.printToPlayer("failure ;(");
-
 		return false;
-	}
-
-	public boolean isConstructedCorrectly(World w, int x, int y, int z) {
-		for (int i = -1; i < 2; i++)
-			for (int j = -1; j < 2; j++)
-				if (!isBlockRight(w, x + i, y - 1, z + j, Blocks.gold_block)) return false;
-		for (int i = -2; i < 3; i++)
-			for (int j = -2; j < 3; j++)
-				if (!isBlockRight(w, x + i, y - 2, z + j, Blocks.stonebrick)) return false;
-
-		return true;
-	}
-
-	public boolean isBlockRight(World w, int x, int y, int z, Block n) {
-		return w.getBlock(x, y, z).equals(n);
 	}
 
 }
