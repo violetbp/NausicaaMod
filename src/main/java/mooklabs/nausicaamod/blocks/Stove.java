6b270f443ba98cb41c241c8c4e3fa8590abc5c86
Emi <cameocellist@gmail.com>
2014-08-30 16:19:48 -0400

    STOVE
    TEA IS A THING AND I HAVE NO IDEA HOW TO DO IT

---
+++ src/main/java/mooklabs/nausicaamod/blocks/Stove.java
@@ -0,0 +1,16 @@
+package mooklabs.nausicaamod.blocks;
+import net.minecraft.block.BlockFurnace;
+/**
+ * EMI IS TOTALLY GOING TO DO TEA
+ * I HAVE NO IDEA WHAT IM DOING
+ * 
+ */
+
+public class Stove extends BlockFurnace {
+
+	protected Stove(boolean p_i45407_1_) {
+		super(p_i45407_1_);
+		// TODO Auto-generated constructor stub
+	}
+//someday there will be code here
+}
