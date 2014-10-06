bc2a6c867f2b2e591b25b6213d82786ecc3d838b
Emi <cameocellist@gmail.com>
2014-10-06 14:21:07 -0400

    tea constructor
    it continues
    -3

--- src/main/java/mooklabs/nausicaamod/tea/Tea.java
+++ src/main/java/mooklabs/nausicaamod/tea/Tea.java
@@ -1,9 +1,19 @@
 package mooklabs.nausicaamod.tea;
+
+import mooklabs.nausicaamod.Main;
+import net.minecraft.item.Item;
+
 /**
  * ive forgotten entirely what i was doing frick
  * well here's commit 1...  only 49 to go!
  */
 
-public class Tea {
+public class Tea extends Item {
+	public Tea() {
+		super();
+		setUnlocalizedName("tea");
+		setTextureName(Main.itemfold + ":tea");
+		this.setMaxStackSize(16);
+	}
 
 }
