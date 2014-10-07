d9e951a5eccf2863d7727a83b8ffb48bae350101
mookie1097 <mookbot@gmail.com>
2014-10-07 11:49:00 -0400

    tea bush work

--- src/main/java/mooklabs/nausicaamod/tea/TeaBush.java
+++ src/main/java/mooklabs/nausicaamod/tea/TeaBush.java
@@ -5,9 +5,14 @@ import net.minecraft.block.material.Material;
 
 /**
  * how do i make a tree
+ * WILL BE ITEMBLOCK PROBLAY
  * @author emilynewman
  *
+ *temporary will get random tea from block
  */
+
+// look here:
+// https://github.com/SlimeKnights/TinkersConstruct/blob/80efde613ac73e98279e1ab8adb1107638f1a0e4/src/main/java/tconstruct/world/blocks/OreberryBush.java
 
 public class TeaBush extends Block {
 
