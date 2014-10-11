93dfeb5a63bd28bc046f30246b12fd3e36d48828
Emi <cameocellist@gmail.com>
2014-10-11 13:58:46 -0400

    okay one one
    26

--- src/main/java/mooklabs/nausicaamod/tea/TeaBush.java
+++ src/main/java/mooklabs/nausicaamod/tea/TeaBush.java
@@ -60,6 +60,115 @@ public class TeaBush extends BlockLeavesBase implements IPlantable {
         }
     }
     */
+	/*also implement later
+	 * @Override
+    public IIcon getIcon (int side, int metadata)
+    {
+        this.setGraphicsLevel(Minecraft.getMinecraft().gameSettings.fancyGraphics);
+        if (field_150121_P)
+        {
+            if (metadata < 12)
+            {
+                return fancyIcons[metadata % 4];
+            }
+            else
+            {
+                return fancyIcons[metadata % 4 + 4];
+            }
+        }
+        else
+        {
+            if (metadata < 12)
+            {
+                return fastIcons[metadata % 4];
+            }
+            else
+            {
+                return fastIcons[metadata % 4 + 4];
+            }
+        }
+    }
+
+    // Bushes are stored by size then type 
+    @Override
+    public int damageDropped (int metadata)
+    {
+        return metadata % 4;
+    }
+
+    // The following methods define a berry bush's size depending on metadata 
+    @Override
+    public AxisAlignedBB getCollisionBoundingBoxFromPool (World world, int x, int y, int z)
+    {
+        int l = world.getBlockMetadata(x, y, z);
+        if (l < 4)
+        {
+            return AxisAlignedBB.getBoundingBox((double) x + 0.25D, y, (double) z + 0.25D, (double) x + 0.75D, (double) y + 0.5D, (double) z + 0.75D);
+        }
+        else if (l < 8)
+        {
+            return AxisAlignedBB.getBoundingBox((double) x + 0.125D, y, (double) z + 0.125D, (double) x + 0.875D, (double) y + 0.75D, (double) z + 0.875D);
+        }
+        else
+        {
+            return AxisAlignedBB.getBoundingBox(x + 0.0625, y, z + 0.0625, (double) x + 0.9375D, (double) y + 0.9375D, (double) z + 0.9375D);
+        }
+    }
+
+    @Override
+    public AxisAlignedBB getSelectedBoundingBoxFromPool (World world, int x, int y, int z)
+    {
+        int l = world.getBlockMetadata(x, y, z);
+        if (l < 4)
+        {
+            return AxisAlignedBB.getBoundingBox((double) x + 0.25D, y, (double) z + 0.25D, (double) x + 0.75D, (double) y + 0.5D, (double) z + 0.75D);
+        }
+        else if (l < 8)
+        {
+            return AxisAlignedBB.getBoundingBox((double) x + 0.125D, y, (double) z + 0.125D, (double) x + 0.875D, (double) y + 0.75D, (double) z + 0.875D);
+        }
+        else
+        {
+            return AxisAlignedBB.getBoundingBox(x, y, z, (double) x + 1.0D, (double) y + 1.0D, (double) z + 1.0D);
+        }
+    }
+
+    @Override
+    public void setBlockBoundsBasedOnState (IBlockAccess iblockaccess, int x, int y, int z)
+    {
+        int md = iblockaccess.getBlockMetadata(x, y, z);
+
+        float minX;
+        float minY = 0F;
+        float minZ;
+        float maxX;
+        float maxY;
+        float maxZ;
+
+        if (md < 4)
+        {
+            minX = minZ = 0.25F;
+            maxX = maxZ = 0.75F;
+            maxY = 0.5F;
+        }
+        else
+
+        if (md < 8)
+        {
+            minX = minZ = 0.125F;
+            maxX = maxZ = 0.875F;
+            maxY = 0.75F;
+        }
+
+        else
+        {
+            minX = minZ = 0.0F;
+            maxX = maxZ = 1.0F;
+            maxY = 1.0F;
+        }
+        setBlockBounds(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+	 */
 	@Override
 	public EnumPlantType getPlantType(IBlockAccess world, int x, int y, int z) {
 		// TODO Auto-generated method stub
