044f968d282130675452c38db5706fd5b107216c
Emi <cameocellist@gmail.com>
2014-10-11 13:48:48 -0400

    descriptions
    ha I'm funny
    20

--- src/main/java/mooklabs/nausicaamod/tea/Teas.java
+++ src/main/java/mooklabs/nausicaamod/tea/Teas.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 public enum Teas {	
 	
-	Black("Black","Black tea is __ and etc",10), Chamomile("Chamomile","",10), Green("Green","",10), Matte("Matte","",10), Mint("Mint","",10), White("White","",10);
+	Black("Black","Black tea is yum and etc",10), Chamomile("Chamomile","good with honey for sick people",10), Green("Green","japanese",10), Matte("Matte","isnt this basically green tea",10), Mint("Peppermint","good for when you're sick!",10), White("White","what are you, racist",10);
 	
 	String name;
 	String desc;
