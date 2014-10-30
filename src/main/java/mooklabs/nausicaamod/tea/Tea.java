8a4e00237ca31f3f79fa0f2af0c32cfe5a88d90d
Emi <cameocellist@gmail.com>
2014-10-30 11:11:13 -0400

    YAY 50
    HAPPY HACKTOBER

--- src/main/java/mooklabs/nausicaamod/tea/Tea.java
+++ src/main/java/mooklabs/nausicaamod/tea/Tea.java
@@ -5,8 +5,8 @@ import mooklabs.nausicaamod.Main;
 import net.minecraft.item.Item;
 
 /**
- * What does this class function as?
- * also why do we have a class named "Tea" and one named "Teas" that's wayyy too similar
+ * 
+ * TODO VIC we should delete this? 
  */
 @Deprecated @Unused
 public class Tea extends Item {
